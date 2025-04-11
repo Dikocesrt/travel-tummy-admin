3eb003f34f72d5c9da6aff339c638fed11452045
Dikocesrt <dikocesrt@gmail.com>
2025-04-11 13:50:41 +0700

    added can access panel for user on production

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,23 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as AuthUser;
 
-class User extends AuthUser
+class User extends AuthUser implements FilamentUser
 {
     use SoftDeletes, HasFactory;
 
     protected $fillable = ["name", "email", "password"];
 
     protected $hidden = ["password"];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
